80946175ab4f5d3c3af1a9cafcf33ac06d15a5bf
John <john@ultralabs.io>
2024-11-14 20:51:47 +0800

    update

--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
